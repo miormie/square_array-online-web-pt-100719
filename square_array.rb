5e692119a319022c303e41e048b48eba72cb940d
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-22 22:49:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
   # your code here
+  #array = []
+  new_array = []
+                                                      #square_array(array)
+  array.each do |number|
+  new_array << (number * number)
+ end
+ return new_array
 end
+
+
+
+
+
+
+#.each return 
+# .each do ||
+#numbers ** num 
+#puts "Primary Color #{color} is #{color.length} letters long." 
